023bb062defd2ba100b4a552c6666c7db0d8bf90
MrBalla <docemulher11@gmail.com>
2025-03-13 10:45:19 +0100

    feat:

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BusModule } from './bus/bus.module';
 
 @Module({
-  imports: [AuthModule, UserModule]
+  imports: [AuthModule, UserModule, BusModule]
 })
 export class AppModule {}
