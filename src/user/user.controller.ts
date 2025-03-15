5ace2d5f801e1fd79b6d49974546926b2a8245b0
MrBalla <docemulher11@gmail.com>
2025-03-15 23:33:22 +0100

    feat(user): add GET endpoint to fetch user by ID

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get, Param, Post, Body, Put, Delete,HttpCode, HttpStatus } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
+import { NotFoundException } from '@nestjs/common';
 @Controller('user')
 export class UserController {
     
   constructor(private readonly userService: UserService) {} 
-  @Post('signup')
+  @Post('')
   @HttpCode(HttpStatus.CREATED)
    async signupUser(@Body() userData: Prisma.UserCreateInput): Promise<void> {
     await this.userService.createUser(userData);
+  }
+  @Get('/:id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    const user = await this.userService.user({ id: Number(id) });
+    if (!user) {
+        throw new NotFoundException(`Usuário com ID ${id} não encontrado`);
+    }
+    return user;
   }
 }
