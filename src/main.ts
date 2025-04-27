7d8c7f621ffc5e7fcb2e3f3844f66126a84063fe
fevunge <developervunge@gmail.com>
2025-04-27 11:54:31 +0000

    feat[swagger] instalar e configurar swagger ui para documentacao da api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin:"*"})
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    patchNestjsSwagger();
+
+    const config = new DocumentBuilder().setTitle('API')
+    .setDescription('API description')
+    .addTag('auth')
+    .addTag('user')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);;
+
+    SwaggerModule.setup('api-doc', app, document);
+
+    app.enableCors({ origin:"*" })
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+    console.log(`Swagger is running on: ${await app.getUrl()}/api-doc`);
 }
 bootstrap();
