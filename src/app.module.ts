daffd119f872d450d2da73b06cc04e5b76541798
MrBalla <docemulher11@gmail.com>
2025-05-09 18:29:09 +0100

    feat: criando o module driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { RoutesModule } from './routes/routes.module';
 import { StopesModule } from './stopes/stopes.module';
+import { DriverModule } from './driver/driver.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule,DatabaseModule, RoutesModule, StopesModule]
+  imports: [AuthModule, UserModule,DatabaseModule, RoutesModule, StopesModule, DriverModule]
 })
 export class AppModule {}
