61c4b5c7d8268e2cdc0039ab674bae35da255bbe
fevunge <developervunge@gmail.com>
2025-05-09 17:20:14 +0000

    feat

--- swagger/swagger.decorator.ts
+++ swagger/swagger.decorator.ts
@@ -1,15 +1,27 @@
-import { applyDecorators } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { ApiOperation, ApiBody } from '@nestjs/swagger';
+import { ZodSchema, ZodObject } from 'zod'; // Importa o tipo do Zod
+import { applyDecorators, Post } from '@nestjs/common';
+import { zodToOpenAPI } from 'nestjs-zod';
 
-interface SwaggerOptions {
-    summary: string;
-    example?: any;
+// Função para garantir que o `example` seja um Zod object (z.object)
+function isZodObject(schema: any): schema is ZodObject<any> {
+  return schema && schema._def && schema._def.typeName === 'ZodObject';
 }
 
-export function SwaggerPost(options: SwaggerOptions) {
-    return applyDecorators(
-        ApiOperation({ summary: options.summary }),
-        ApiBody({ type: options.example }),
-    );
-}
+// Decorator SwaggerPost
+export function SPost( route: string, schema: ZodObject<any>) {
+  if (!isZodObject(schema)) {
+    throw new Error('The schema must be a Zod object (z.object)');
+  }
 
+  return applyDecorators(
+    Post(route),
+    ApiBody({ 
+        schema: {
+            type: 'object',
+            properties: zodToOpenAPI(schema).properties,
+            required: Object.keys(schema.shape)
+        } 
+    })
+  );
+}
