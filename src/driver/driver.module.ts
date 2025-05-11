2d323744783dadcdbd73a8e40fbadb7537b4c85f
MrBalla <docemulher11@gmail.com>
2025-05-11 08:07:45 +0100

    feat:  criando e configiurando module driver

--- src/driver/driver.module.ts
+++ src/driver/driver.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { DriverLocationGateway } from 'src/gateways/driver-location.gateway';
 
 @Module({
+  imports: [DatabaseModule, forwardRef(() => AuthModule), UserModule],
   controllers: [DriverController],
-  providers: [DriverService]
+  providers: [DriverService, DriverLocationGateway,],
+  exports:[DriverService]
 })
 export class DriverModule {}
