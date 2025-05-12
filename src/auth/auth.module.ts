ff43c67992d2ce63b7eb2ba968cd52713cd8c245
Dário Silva <dariosilva13222@gmail.com>
2025-05-12 16:43:24 +0100

    chore: imports DriverModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
+import { DriverModule } from 'src/driver/driver.module';
 
 @Module({
   imports: [forwardRef(()=>UserModule), JwtModule.register({
     global: true,
     secret: process.env.SECRET,
     signOptions: { expiresIn: '1d' },
-  })],
+  }),forwardRef(()=> DriverModule)],
   controllers: [AuthController],
   providers: [AuthService, AuthGuard],
   exports: [AuthService, AuthGuard],
