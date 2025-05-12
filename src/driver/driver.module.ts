2788054e9caf45eabf3b26d481545378cbb62a18
Dário Silva <dariosilva13222@gmail.com>
2025-05-12 16:44:10 +0100

    chore: imports Bus module and adds driver and alert gateway's as providers

--- src/driver/driver.module.ts
+++ src/driver/driver.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { DriverLocationGateway } from 'src/gateways/driver-location.gateway';
+import { AlertsChannelGateway } from 'src/gateways/alert-channel.gateway';
+import { BusModule } from 'src/bus/bus.module';
 
 @Module({
+  imports:[DatabaseModule,forwardRef(()=> AuthModule),BusModule],
   controllers: [DriverController],
-  providers: [DriverService]
+  providers: [DriverService,DriverLocationGateway,AlertsChannelGateway],
+  exports:[DriverService]
 })
 export class DriverModule {}
