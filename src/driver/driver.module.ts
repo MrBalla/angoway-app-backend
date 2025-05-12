69aaad82739f00fef061d5d6df420041b3131306
MrBalla <docemulher11@gmail.com>
2025-05-12 13:18:26 +0100

    feat: exportando e importando o motorista

--- src/driver/driver.module.ts
+++ src/driver/driver.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from 'src/database/database.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { DriverLocationGateway } from 'src/gateways/driver-location.gateway';
+import { BusModule } from 'src/bus/bus.module';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => AuthModule), UserModule],
+  imports: [DatabaseModule, forwardRef(() => AuthModule), UserModule, BusModule],
   controllers: [DriverController],
   providers: [DriverService, DriverLocationGateway,],
   exports:[DriverService]
