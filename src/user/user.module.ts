7e43ba773ac45cca97ebc8f16a316c279d48a8a8
Dário Silva <dariosilva13222@gmail.com>
2025-05-13 18:57:45 +0100

    chore: imports driver module inside user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,9 +8,10 @@ import { AuthModule } from 'src/auth/auth.module';
 import { DriverLocationGateway } from 'src/gateways/driver-location.gateway';
 import { BusModule } from 'src/bus/bus.module';
 import { AlertsChannelGateway } from 'src/gateways/alert-channel.gateway';
+import { DriverModule } from 'src/driver/driver.module';
 
 @Module({
-  imports:[DatabaseModule,forwardRef(()=>AuthModule),BusModule],
+  imports:[DatabaseModule,forwardRef(()=>AuthModule),BusModule,DriverModule],
   controllers: [UserController],
   providers: [UserService,DriverLocationGateway,AlertsChannelGateway],
   exports:[UserService]
