56bed8eef0d6658f45b80e616e205228912473b1
Dário Silva <dariosilva13222@gmail.com>
2025-05-13 20:51:04 +0100

    feat: creates an object to send additional informations about the alert to all clients

--- src/gateways/alert-channel.gateway.ts
+++ src/gateways/alert-channel.gateway.ts
@@ -10,6 +10,9 @@ import { Socket } from 'socket.io';
 import { AlertNotification } from 'src/types/alert.notification';
 import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { DriverService } from 'src/driver/driver.service';
+import { BusService } from 'src/bus/bus.service';
+import { RoutesService } from 'src/routes/routes.service';
 
 @WebSocketGateway({
   cors: {
@@ -23,7 +26,12 @@ export class AlertsChannelGateway
 {
   private logger = new Logger('AlertsChannelGateway');
 
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly driverService: DriverService,
+    private readonly busService: BusService,
+    private readonly routesService:RoutesService,
+  ) {}
 
   async handleConnection(client: Socket) {
     try {
@@ -55,6 +63,38 @@ export class AlertsChannelGateway
       return;
     }
 
-    client.broadcast.emit('alertNotificationsUpdate', alert);
+    const driver = await this.driverService.driver({ id: alert.driverId });
+
+    if (!driver) {
+      this.logger.warn('No Driver found !');
+      return;
+    }
+
+    const driverBus = await this.busService.findBusByDriverId(driver.id);
+
+    if (!driverBus) {
+      this.logger.warn(`No Bus was found for ${driver.name}!`);
+      return;
+    }
+
+    const busRoute = await this.routesService.findOne(driverBus.routeId)
+
+    if (!busRoute) {
+      this.logger.warn(`No Route was found for Bus ${driverBus.nia}!`);
+      return;
+    }
+
+    const adminAlert = {
+      driverName: driver.name,
+      type: alert.type,
+      driverId: alert.driverId,
+      message: alert.message,
+      loc: alert.loc,
+      timestamp: alert.timestamp, //convert this
+      busNIA: driverBus.nia,
+      route: busRoute.name
+    };
+
+    client.broadcast.emit('alertNotificationsUpdate', adminAlert);
   }
 }
