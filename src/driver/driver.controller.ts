d93a963bed3c9ec4b6a10f88986448d91245bd07
MrBalla <docemulher11@gmail.com>
2025-05-16 10:40:30 +0100

    feat: terminando o controllers

--- src/driver/driver.controller.ts
+++ src/driver/driver.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { DriverService } from './driver.service';
 import { Prisma } from '@prisma/client';
 import { ResponseBody } from 'src/types/response.body';
@@ -62,6 +62,101 @@ export class DriverController {
         return driver;
     }
 
-    
+  @Patch('/update/:id')
+  @HttpCode(HttpStatus.OK)
+  async updateDriver(@Param('id') id: string, @Body() updateDriverData: Prisma.DriverUpdateInput,): Promise<ResponseBody> {
+    const driver = await this.driverService.updateDriver({
+      where: { id: Number(id) },
+      data: updateDriverData,
+    });
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: 'Motorista atualizado com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
+
+  @Patch('/password/:id')
+  @HttpCode(HttpStatus.OK)
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordData: { password: string }): Promise<ResponseBody> {
+    const driver = await this.driverService.updatePassword({
+      where: { id: Number(id) },
+      data: { password: updatePasswordData.password },
+    });
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: 'Senha atualizada com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.OK)
+  async deleteDriver(@Param('id') id: string): Promise<ResponseBody> {
+    await this.driverService.deleteDriver({ id: Number(id) });
+    return {
+      message: 'Motorista deletado com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
+
+  @Patch('/status/:id')
+  @HttpCode(HttpStatus.OK)
+  async updateStatus( @Param('id') id: string, @Body() updateStatusData: 
+  { status: 'AVAILABLE' | 'ON_ROUTE' | 'OFFLINE' } ): Promise<ResponseBody> {
+    const driver = await this.driverService.updateStatus(Number(id), updateStatusData.status);
+    if (!driver) {
+      throw new NotFoundException(`Motorista com ID ${id} não encontrado`);
+    }
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: 'Status do motorista atualizado com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
+
+  @Patch('/verify/:id')
+  @HttpCode(HttpStatus.OK)
+  async verifyDriver(@Param('id') id: string): Promise<ResponseBody> {
+    const driver = await this.driverService.verifyDriver({ id: Number(id) });
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: `Motorista ${driver.isVerified ? 'verificado' : 'desverificado'} com sucesso`,
+      code: HttpStatus.OK,
+    };
+  }
+
+  @Patch('/unassign-bus/:id')
+  @HttpCode(HttpStatus.OK)
+  async unassignBus(@Param('id') id: string): Promise<ResponseBody> {
+    const driver = await this.driverService.unassignBus(Number(id));
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: 'Atribuição de autocarro removida com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
+
+
+//Acho que não vamos usar, mas deixa só deixar aqui por preucação
+  @Patch('/location/:id')
+  @HttpCode(HttpStatus.OK)
+  async updateLocation(
+    @Param('id') id: string,
+    @Body() updateLocationData: { latitude: number; longitude: number },
+  ): Promise<ResponseBody> {
+    const driver = await this.driverService.updateLocation(
+      Number(id),
+      updateLocationData.latitude,
+      updateLocationData.longitude,
+    );
+    const { password, ...driverWithoutPassword } = driver;
+    return {
+      message: 'Localização do motorista atualizada com sucesso',
+      code: HttpStatus.OK,
+    };
+  }
 
 }
