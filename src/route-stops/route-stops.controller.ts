cbd33a7d92311979605a3673a488f4ae75d75cb5
MrBalla <docemulher11@gmail.com>
2025-05-18 17:48:24 +0100

    feat criando o route controller

--- src/route-stops/route-stops.controller.ts
+++ src/route-stops/route-stops.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { RouteStopsService } from './route-stops.service';
 
 @Controller('route-stops')
-export class RouteStopsController {}
+export class RouteStopsController {
+    constructor(private readonly routeStopsService: RouteStopsService) {}
+    
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async createRouteStops(@Body() data: {
+        routeId: number;
+        stopId: number;
+        order: number
+    }) {
+        const added =await this.routeStopsService.addToRoute(
+            data.routeId,
+            data.stopId,
+            data.order
+        );
+        return{
+            message: 'adicionada com sucesso',
+            data: added,
+        }
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.OK)
+    async deleteRouteStops(@Param('id')id: string) {
+        await this.routeStopsService.removeStopFromRoute(Number(id));
+        return {
+            message: 'removida com sucesso',
+        }
+    }
+
+    @Patch('ordenar/:id')
+    @HttpCode(HttpStatus.OK)
+    
+}
