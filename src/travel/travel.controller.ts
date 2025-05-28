4e13c916a66ed54e7e00a1747c9d236c8103e71e
fevunge <developervunge@gmail.com>
2025-05-28 01:34:31 +0000

    feat: Criar um endpoint que retorna o total faturado nos últimos 7 dias.

--- src/travel/travel.controller.ts
+++ src/travel/travel.controller.ts
@@ -7,9 +7,11 @@ import {
   Param,
   Delete,
   Res,
+  Query,
 } from '@nestjs/common';
 import { TravelService } from './travel.service';
 import { countMonthly } from '../types/count-monthly.details';
+import { weeklyEarnings } from '../type/weekly-earning.response'
 
 @Controller('travel')
 export class TravelController {
@@ -39,6 +41,26 @@ export class TravelController {
 
     res.send(buffer);
   }
+    @Get('weekly-earnings')
+    async weeklyEarnings(
+        @Query('date') date?: string, // YYYY-MM-DD
+        @Query('week') week?: string, // YYYY-WW
+    ): Promise<weeklyEarnings> {
+        const dayWeek: string;
+    }
+
+    @Get('weekly-earnings/driver')
+    async weeklyEarningsByDriver() {
+
+    }
+
+    @Get('weekly-earnings/route')
+    async weeklyEarningsByRoute() {
+
+    }
+
+    @Get('weekly-')
+
 
   @Get()
   findAll() {
