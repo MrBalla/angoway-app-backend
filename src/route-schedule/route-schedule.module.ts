5db54bee3a29ad6c45838b4201609bd050ac38e1
Dário Silva <dariosilva13222@gmail.com>
2025-06-05 19:33:11 +0100

    chore: imports Database and Routes modules chore: exports RouteScheduleModule and RouteScheduleService objects

--- src/route-schedule/route-schedule.module.ts
+++ src/route-schedule/route-schedule.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RouteScheduleService } from './route-schedule.service';
 import { RouteScheduleController } from './route-schedule.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { RoutesModule } from 'src/routes/routes.module';
 
 @Module({
+  imports:[DatabaseModule,RoutesModule],
   controllers: [RouteScheduleController],
   providers: [RouteScheduleService],
+  exports:[RouteScheduleModule,RouteScheduleService]
 })
 export class RouteScheduleModule {}
