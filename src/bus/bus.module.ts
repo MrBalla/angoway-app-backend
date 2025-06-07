8a2908dbf50e49b74a760f132e17b24f807032be
Dário Silva <dariosilva13222@gmail.com>
2025-06-07 13:39:20 +0100

    chore: import Travel model module

--- src/bus/bus.module.ts
+++ src/bus/bus.module.ts
@@ -3,9 +3,10 @@ import { BusService } from './bus.service';
 import { BusController } from './bus.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { RoutesModule } from 'src/routes/routes.module';
+import { TravelModule } from 'src/travel/travel.module';
 
 @Module({
-  imports:[DatabaseModule,RoutesModule],
+  imports:[DatabaseModule,RoutesModule,TravelModule],
   providers: [BusService],
   controllers: [BusController],
   exports: [BusModule,BusService],
